dfe6f0e25605e99e57dac3f83e11fe3afb44d864
Piers Williams <solar_1992@hotmail.co.uk>
2016-06-13 15:29:16 +0100

    Documented the deck and the Hanabi classes

--- src/main/java/uk/ac/essex/csee/iggi/hanabi/Deck.java
+++ src/main/java/uk/ac/essex/csee/iggi/hanabi/Deck.java
@@ -9,7 +9,13 @@ public class Deck {
 	public Deck() {
 		this.cards = new Stack<Card>();
 	}
-	
+
+    /**
+     * Provides a clone of the given deck
+     *
+     * A shallow copy - but the cards themselves are immutable so no problem.
+     * @param deck The given deck to be cloned
+     */
 	public Deck(Deck deck) {
 		this.cards = (Stack<Card>)deck.cards.clone();
 	}
@@ -17,15 +23,27 @@ public class Deck {
 	public void shuffle() {
 		
 	}
-	
+
+    /**
+     * Gets and removes the top card from the deck
+     * @return The card that was on top of the deck
+     */
 	public Card getTopCard() {
 		return cards.pop();
 	}
-	
+
+    /**
+     * Gets the number of cards left in the deck
+     * @return int number of cards left
+     */
 	public int getCardsLeft() {
 		return cards.size();
 	}
 
+    /**
+     * Are there any cards left in the deck?
+     * @return boolean are there any cards left?
+     */
 	public boolean hasCardsLeft() {
 		return cards.isEmpty();
 	}
--- src/main/java/uk/ac/essex/csee/iggi/hanabi/Hanabi.java
+++ src/main/java/uk/ac/essex/csee/iggi/hanabi/Hanabi.java
@@ -9,11 +9,17 @@ import java.util.TreeMap;
 public class Hanabi {
 	private Deck deck;
 	private int handSize;
+    // Player number to list of cards in their hand
 	private Map<Integer, List<Card>> players;
 	private int lives = 3;
 	private int information = 8;
 	private Map<CardColour, Integer> table;
-	
+
+    /**
+     * Constructor
+     * @param numPlayers Number of players in the game
+     * @param handSize The number of cards in each hand
+     */
 	public Hanabi(int numPlayers, int handSize) {
 		this.deck = new Deck();
 		this.players = new TreeMap<>();
@@ -26,7 +32,12 @@ public class Hanabi {
 			players.put(i, drawHand(handSize));
 		}
 	}
-	
+
+    /**
+     * Draws a hand of given size from the deck
+     * @param size The size of the hand
+     * @return The list of cards retrieved from the deck
+     */
 	public List<Card> drawHand(int size) {
 		List<Card> cards = new ArrayList<Card>();
 		for (int i=0; i<size;i++) {
@@ -35,19 +46,37 @@ public class Hanabi {
 		
 		return cards;
 	}
-	
+
+    /**
+     * Clones a game of Hanabi
+     * @param hanabi
+     */
 	public Hanabi(Hanabi hanabi) {
 		this.deck = new Deck(hanabi.deck);
+        //TODO  This will be a possible issue - players will be a deep clone but the list of cards in each hand will be the same - in one game a discard will affect another
 		this.players = new TreeMap<>(hanabi.players);
 		this.table = new TreeMap<>(hanabi.table);
 		this.lives = hanabi.lives;
 		this.information = hanabi.information;
 	}
 
+    /**
+     * Is the game over?
+     * @return boolean wether the game is over or not
+     */
 	public boolean isOver() {
 		return lives <= 0 && deck.hasCardsLeft();
 	}
-	
+
+    /**
+     * Gets the indices of every card that matches the ineger provided
+     *
+     * For example getting the index of each card that is a 2
+     * @param whoIsBeingTold The player that is being told the information about
+     * @param number The number that is in the query
+     * @return The list of indices of each card that matches the query. Will be null if the query isn't allowed
+     * and empty if there were no cards matching the query
+     */
 	public List<Integer> getAllInteger(Integer whoIsBeingTold, Integer number) {
 		if (information < 0) {
 			return null; //nope
@@ -69,7 +98,16 @@ public class Hanabi {
 		
 		return cardPos;
 	}
-	
+
+    /**
+     * Gets the indices of every card that matches the CardColour provided
+     *
+     * For example getting the index of each card that is a White
+     * @param whoIsBeingTold The player that is being told the information about
+     * @param colour The CardColour that is in the query
+     * @return The list of indices of each card that matches the query. Will be null if the query isn't allowed
+     * and empty if there were no cards matching the query
+     */
 	public List<Integer> getAllColour(Integer whoIsBeingTold, CardColour colour) {
 		if (information < 0) {
 			return null; //nope
@@ -91,7 +129,15 @@ public class Hanabi {
 		
 		return cardPos;
 	}
-	
+
+    /**
+     * Gets the next card
+     *
+     * TODO what was this function for?
+     * @param player
+     * @param card
+     * @return
+     */
 	public Card getNextCard(Integer player, Card card) {
 		List<Card> cards = players.get(player);
 		assert players.containsKey(player) : "player does not exist";
@@ -100,7 +146,14 @@ public class Hanabi {
 		
 		return deck.getTopCard();
 	}
-	
+
+    /**
+     * Discard a given card
+     * @param player The player that is discarding the card
+     * @param card The card that the player is discarding
+     * @return The card that was taken from the deck after the discard. TODO Why are we doing this?
+     * TODO Why not place this directly in the hand
+     */
 	public Card discard(Integer player, Card card) {
 		Card nextCard = getNextCard(player, card);
 		
@@ -110,7 +163,13 @@ public class Hanabi {
 		
 		return nextCard;
 	}
-	
+
+    /**
+     * Plays a card onto the table and returns the next card for your hand
+     * @param player The player that is playing the card
+     * @param card The card that the player is playing
+     * @return The card that is going back into the hand of the player
+     */
 	public Card play(Integer player, Card card) {
 		Card nextCard = getNextCard(player, card);
 		
@@ -126,21 +185,39 @@ public class Hanabi {
 		
 		return nextCard;
 	}
-	
+
+    /**
+     * Gets a view of the provided player's cards
+     * @param player The player that we want to look at the cards of
+     * @return The cards - an unmodifiableList
+     */
 	public List<Card> getCards(Integer player) {
 		return Collections.unmodifiableList(players.get(player));
 	}
-	
+
+    /**
+     * Gets the current number of the colour that is on the table
+     * @param colour The colour that we are asking about
+     * @return The number of the highest card
+     */
 	public Integer getCurrentTable(CardColour colour) {
 		Integer currentScore = table.get(colour);
 		currentScore = currentScore == null ? 0 : currentScore;
 		return currentScore;
 	}
-	
+
+    /**
+     * Returns the entire table as an unmodifiable map
+     * @return The map
+     */
 	public Map<CardColour, Integer> getTable() {
 		return Collections.unmodifiableMap(table);
 	}
-	
+
+    /**
+     * Returns the number of lives for the game left
+     * @return
+     */
 	public int getLives() {
 		return lives;
 	}
