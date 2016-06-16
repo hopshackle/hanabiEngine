b77c049bab8c151d9f9c083814f3ba33d4b89597
Piers Williams <solar_1992@hotmail.co.uk>
2016-06-16 09:04:29 +0100

    Started work on the game engine side of things

--- src/main/java/uk/ac/essex/csee/iggi/hanabi/GameEngine.java
+++ src/main/java/uk/ac/essex/csee/iggi/hanabi/GameEngine.java
@@ -1,25 +1,38 @@
 package uk.ac.essex.csee.iggi.hanabi;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class GameEngine {
 
 	public void playGame(Player ... players) {
 		Hanabi hanabi = new Hanabi(players.length, 5);
-		
-		for (Player player : players) {
-			Move move = player.getMove(precepts);
-			
-			switch (move.getType()) {
-				case DISCARD:
-					precepts.add(hanabi)
-					
-				case PLAY:
-				case TELL:
-			}
-			
-		}
+
+        // Map of player to all moves since player's last turn
+        Map<Player, List<Move>> percepts = new HashMap<>();
+        for(Player player : players){
+            percepts.put(player, new ArrayList<Move>());
+        }
+        while(!hanabi.isOver()) {
+            for (Player player : players) {
+                Move move = player.getMove(percepts.get(player));
+                // Clear out the information in the percepts list for this player
+                percepts.get(player).clear();
+                switch (move.getType()) {
+                    case DISCARD:
+                    case PLAY:
+                    case TELL:
+                }
+
+                for(Player other : players){
+                    // TODO Decide if we should exclude our own move?
+                    percepts.get(other).add(move);
+                }
+
+            }
+        }
 	}
 
 }
--- src/main/java/uk/ac/essex/csee/iggi/hanabi/Move.java
+++ src/main/java/uk/ac/essex/csee/iggi/hanabi/Move.java
@@ -1,5 +1,8 @@
 package uk.ac.essex.csee.iggi.hanabi;
 
+/**
+ * A move object - contains all the information needed to execute a move
+ */
 public class Move {
 	public MoveType type;
 	public CardColour colour;
