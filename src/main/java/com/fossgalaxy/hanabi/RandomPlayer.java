94fed7ca5c9c92509c76f4f46e98292b5b1abee0
Joseph Walton-Rivers <joseph@webpigeon.me.uk>
2016-07-26 17:54:16 +0100

    a randomish player

---
+++ src/main/java/com/fossgalaxy/hanabi/RandomPlayer.java
@@ -0,0 +1,33 @@
+package com.fossgalaxy.hanabi;
+
+import java.util.Random;
+
+//TODO I need to know hand size, number of players and my playerID
+public class RandomPlayer implements Player {
+	private Random random;
+	
+	public RandomPlayer() {
+		this.random = new Random();
+	}
+	
+	@Override
+	public void sendMessage(String msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getAction() {
+		int n = random.nextInt(2);
+		int slot = random.nextInt(5);
+		
+		if (n == 0) {
+			return "PLAY "+slot;
+		} else if (n == 1) {
+			return "DISCARD "+slot;
+		}
+		
+		return "PLAY 0";
+	}
+
+}
