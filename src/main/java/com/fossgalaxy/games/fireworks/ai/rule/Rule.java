ef868966a22abf047abf0a0b92a6d5fd39dd7384
Joseph Walton-Rivers <jwalto@essex.ac.uk>
2016-10-18 14:43:57 +0100

    java 1.8 rules - allow lambdas and make canfire a default method

--- src/main/java/com/fossgalaxy/games/fireworks/ai/rule/Rule.java
+++ src/main/java/com/fossgalaxy/games/fireworks/ai/rule/Rule.java
@@ -3,9 +3,14 @@ package com.fossgalaxy.games.fireworks.ai.rule;
 import com.fossgalaxy.games.fireworks.state.GameState;
 import com.fossgalaxy.games.fireworks.state.actions.Action;
 
+@FunctionalInterface
 public interface Rule {
 
-	boolean canFire(int playerID, GameState state);
+	default boolean canFire(int playerID, GameState state){
+		Action returnedAction = execute(playerID, state);
+		return returnedAction != null;
+	}
+
 	Action execute(int playerID, GameState state);
 
 }
