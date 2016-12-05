2f1c019e234f756fcd796ba7206bceff09075fc0
Joseph Walton-Rivers <jwalto@essex.ac.uk>
2016-12-05 17:41:36 +0000

    document game stats class

--- src/main/java/com/fossgalaxy/games/fireworks/GameStats.java
+++ src/main/java/com/fossgalaxy/games/fireworks/GameStats.java
@@ -1,14 +1,43 @@
 package com.fossgalaxy.games.fireworks;
 
 public class GameStats {
+    /**
+     * A unique ID for this game.
+     */
     public final String gameID;
+
+    /**
+     * The number of players that were in this game
+     */
     public final int nPlayers;
 
+    /**
+     * The score at the final move of the game.
+     */
     public final int score;
+
+    /**
+     * The number of lives remaining at the end of the game.
+     *
+     * This value being zero indicates that the game ended due to the player's losing their lives
+     */
     public final int lives;
+
+    /**
+     * The number of moves made in the game
+     */
     public final int moves;
 
+    /**
+     * The number of information tokens remaining at the end of the game
+     */
     public final int infomation;
+
+    /**
+     * The number of illegal (cheating) moves made during this game.
+     *
+     * If this value is greater than zero, then at least one agent in the game should be considered buggy.
+     */
     public final int disqal;
 
     public GameStats(String gameID, int players, int score, int lives, int moves, int information, int disqual) {
