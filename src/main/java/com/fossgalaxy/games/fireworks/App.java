9e80c59f6976bc991cadc26acf1fb987b4bc340b
Joseph Walton-Rivers <jwalto@essex.ac.uk>
2016-12-12 14:40:34 +0000

    generate combined jar for ease of testing

--- src/main/java/com/fossgalaxy/games/fireworks/App.java
+++ src/main/java/com/fossgalaxy/games/fireworks/App.java
@@ -37,7 +37,7 @@ public class App {
         System.out.println("Start");
 
         for (int run = 0; run < 10_000; run++) {
-            GameStats stats = playMixed("iggi", "iggi");
+            GameStats stats = playMixed("mctsND", "iggi");
             sum += stats.score;
             games++;
         }
