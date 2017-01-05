ed799a67d9912e515753dd60cf41406a80fbfef3
Piers Williams <solar_1992@hotmail.co.uk>
2017-01-05 16:12:57 +0000

    Corrected number of runs for van

--- src/main/scripts/validation/van.sh
+++ src/main/scripts/validation/van.sh
@@ -3,7 +3,7 @@ set -e
 #params
 export GENERATOR_ARGS="vandenbergh 3"
 export FIREWORKS_REPEAT_COUNT=1
-export FIREWORKS_NUM_SEEDS=100000
+export FIREWORKS_NUM_SEEDS=10000
 export RESULT_DIR="vandenbergh"
 
 ./src/main/scripts/validation/buildAndExecuteValidation.sh
