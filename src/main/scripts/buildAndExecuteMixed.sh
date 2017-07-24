1dc7611e474d51a7c5530130c69c317ad2896173
Joseph Walton-Rivers <jwalto@essex.ac.uk>
2017-07-24 14:17:13 +0100

    make sure version number is correct in buildscripts

--- src/main/scripts/buildAndExecuteMixed.sh
+++ src/main/scripts/buildAndExecuteMixed.sh
@@ -8,7 +8,7 @@ set -e
 
 VERSION=0.1.0
 SUBJECT=webpigeon
-JAR_FILE=fireworks-0.2-SNAPSHOT-jar-with-dependencies.jar
+JAR_FILE=fireworks-0.2.2-SNAPSHOT-jar-with-dependencies.jar
 JOB_FILE=mixed.job
 JOB_CHEAT_FILE=mixedCheat.job
 GENERATOR_CLASS=com.fossgalaxy.games.fireworks.cluster.GenerateGames
