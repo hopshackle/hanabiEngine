aa1d6276c40bc601adbb4abf7aec6948159917bb
hopshackle <james@janigo.co.uk>
2018-05-21 09:42:25 +0100

    Added MCTSInfoSetPolicy
    Uses MCTSInfoSet, but with a specified policy in rollout

--- src/main/java/com/fossgalaxy/games/fireworks/ai/AppParamSearch.java
+++ src/main/java/com/fossgalaxy/games/fireworks/ai/AppParamSearch.java
@@ -17,33 +17,47 @@ import java.util.stream.Collectors.*;
 
 /**
  * Game runner for testing.
- *
+ * <p>
  * This will run a bunch of games with your agent so you can see how it does.
  */
-public class AppParamSearch
-{
+public class AppParamSearch {
 
     private static DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("HH:mm:ss 'on' dd-LLL");
-    public static void main( String[] args )
-    {
+
+    public static void main(String[] args) {
         String policy = (args.length == 0) ? "outer" : args[0];
+        String basePolicy;
+        String rolloutPolicy = "";
         String C_options = (args.length < 2) ? "1.0" : args[1];
-        String R_options = (args.length < 3) ? "100"  : args[2];
+        String R_options = (args.length < 3) ? "100" : args[2];
         String D_options = (args.length < 4) ? "1" : args[3];
         String T_options = (args.length < 5) ? "1000" : args[4];
         List<String> agentNames = new ArrayList<>();
+        String[] corePolicy = policy.split("\\|");
+        if (corePolicy.length == 1) {
+            basePolicy = corePolicy[0];
+        } else {
+            rolloutPolicy = corePolicy[1];
+            basePolicy = corePolicy[0];
+        }
         List<String> explorationC = Arrays.stream(C_options.split("\\|")).collect(Collectors.toList());
-    //            Arrays.asList("0.1", "0.3", "1.0", "3.0");
+        //            Arrays.asList("0.1", "0.3", "1.0", "3.0");
         List<String> timeLimit = Arrays.stream(T_options.split("\\|")).collect(Collectors.toList());
-                //Arrays.asList("100", "300", "1000", "3000", "10000", "30000");
+        //Arrays.asList("100", "300", "1000", "3000", "10000", "30000");
         List<String> treeDepth = Arrays.stream(D_options.split("\\|")).collect(Collectors.toList());
         List<String> rollouts = Arrays.stream(R_options.split("\\|")).collect(Collectors.toList());
 
         for (String T : timeLimit) {
             for (String C : explorationC) {
-                for (String R : rollouts){
+                for (String R : rollouts) {
                     for (String D : treeDepth) {
-                        agentNames.add("mctsPolicy[" + C + ":" + R + ":" + D + ":" + T + ":" + policy + "]");
+                        String agentName = (basePolicy + "[" + C + ":" + R + ":" + D + ":" + T);
+                        if (rolloutPolicy.equals("")) {
+                            agentName += "]";
+                        } else {
+                            agentName += ":" + rolloutPolicy + "]";
+                        }
+                        agentNames.add(agentName);
                     }
                 }
             }
@@ -61,12 +75,12 @@ public class AppParamSearch
         Random random = new Random();
         StatsSummary statsSummary = new BasicStats();
 
-        for (int i=0; i<numGames; i++) {
-   //         System.out.println("Game " + i + " starting");
+        for (int i = 0; i < numGames; i++) {
+            //         System.out.println("Game " + i + " starting");
             GameRunner runner = new GameRunner("test-game", numPlayers);
 
             //add your agents to the game
-            for (int j=0; j<numPlayers; j++) {
+            for (int j = 0; j < numPlayers; j++) {
                 // the player class keeps track of our state for us...
                 Player player = new AgentPlayer(agentDescriptor, AgentUtils.buildAgent(agentDescriptor));
                 runner.addPlayer(player);
